99ecc265a8f6669bbc0e5803034b0156a1af5417
DuendeInexisten <DuendeInexistente@gmail.com>
2020-10-08 03:42:51 +0000

    Added city shuffle selection menu

--- main.lua
+++ main.lua
@@ -13,278 +13,60 @@ races = {'Altmer',
 	'Orc',
 	'Redguard' }
 
-cities = {
-	'Necrom',
-	'Ebon Tower',
-	'Port Telvannis',
-	'Firewatch',
-	'Gah Sadrith',
-	'Almas Thirr',
-	'Helnim',
-	'Aimrah',
-	'Bahrammu',
-	'Bal Oyra',
-	'Bosmora',
-	'Dondril',
-	'Enamor Dayn',
-	'Gorne',
-	'Ildrim',
-	'Indal-ruhn',
-	'Llothanis',
-	'Molagreahd',
-	'Meralag',
-	'Ranyon-ruhn',
-	'Sadas Plantation',
-	'Sailen',
-	'Tel Mothrivra',
-	'Mainland Tribunal Temple',
-	'Tower of Tel Mothrivra',
-	'Tel Muthada',
-	'Vhul',
-	'Ammar',
-	'Cephorad Keep',
-	'Dreynim Spa',
-	'Erethan Plantation',
-	'Akamora',
-	'Fort Umbermoth',
-	'Nethril Plantation',
-	'Mephalain Mountains',
-	'Oran Plantation',
-	'Roa Dyr',
-	'Tel Aranyon',
-	'Master Mithras',
-	'Tel Narrusa',
-	'Narrusa Darythi',
-	'Vathras Plantation',
-	'Windmoth Legion Fort',
-	'Great House Telvanni',
-	'Acre of Saint Meris',
-	'Sacred Lands',
-	'Adurin-Ouaka',
-	'Akamora Mines',
-	'Alendu Plantation',
-	'Areno Varos House',
-	'Ashamul',
-	'Baldrahn',
-	'Boethian Mountains Lighthouse',
-	'Sarvanni',
-	'Delos Deralins House',
-	'Denaven Manor',
-	'Edhryn Farothrans Lighthouse',
-	'Sea of Ghosts',
-	'Evos',
-	'Felms Ithul',
-	'Galam and Mehra Telas House',
-	'Gol Mok',
-	'Halaran Camp',
-	'Harethim Manor',
-	'Bisandryon',
-	'Helerans Lodge',
-	'Hla Bulor',
-	'Hlersis',
-	'The Inn Between',
-	'Kellidan',
-	'Lan Orethan Outpost',
-	'Llaran Manor',
-	'Lumber Camp',
-	'Melethi Lighthouse',
-	'Lake Boethiah',
-	'Mundrethi Plantation',
-	'Slave Market',
-	'Orelu Plantation',
-	'Tel Ouada',
-	'Rethil Manor',
-	'Rilsoan',
-	'Romithren Monastery',
-	'Sacred Lands Sanctuary',
-	'Sadavel Plantation',
-	'Dreynim',
-	'Salvu Manor',
-	'Sarvanni Molecrab Farm',
-	'Molecrab',
-	'Boethiahs Spine',
-	'Sassanibats Yurt',
-	'Seitur',
-	'Nedothril',
-	'Sennahanib Camp',
-	'Serynthul',
-	'Old Ebonheart',
-	'Sinramen Monastery',
-	'Soramil and Rothera Galdavos House',
-	'Tahvel',
-	'Mephalan Vales',
-	'Tel Oren',
-	'Inner Sea',
-	'Mistress Faruna',
-	'Telvanni Library',
-	'Telvanni Waystation',
-	'Alt Bosara',
-	'Thalotheran Manor',
-	'Tharonen Manor',
-	'Lan Orethan',
-	'Tilmeth Manor',
-	'Alt Orethan',
-	'Tomaril Manor',
-	'Veralan Farm',
-	'Marog',
-	'Verulas Pass',
-	'Vlars Manor',
-	'Ald-ruhn',
-	'Balmora',
-	'Sadrith Mora',
-	'Vivec ',
-	'Vvardenfell',
-	'Vivec Foreign',
-	'Vivec Hlaalu',
-	'House Hlaalu',
-	'Vivec Redoran',
-	'House Redoran',
-	'Vivec Telvanni',
-	'House Telvanni',
-	'Vivec Arena',
-	'Vivec St. Olms',
-	'Vivec St. Delyn',
-	'Tribunal Temple',
-	'Vivec Temple',
-	'Vivec Ministry',
-	'Ordinator',
-	'Palace of Vivec',
-	'Ald Velothi',
-	'Caldera',
-	'West Gash',
-	'Dagon Fel',
-	'Gnaar Mok',
-	'Gnisis',
-	'Hla Oad',
-	'Khuul',
-	'Maar Gan',
-	'Molag Mar',
-	'Pelagiad',
-	'Seyda Neen',
-	'Suran',
-	'Tel Aruhn',
-	'Tel Branora',
-	'Tel Fyr',
-	'Tel Mora',
-	'Tel Vos',
-	'Vos',
-	'Redoran',
-	'Bal Isra',
-	'Buckmoth Legion Fort',
-	'Ebonheart',
-	'Imperial',
-	'Moonmoth Legion Fort',
-	'Wolverine Hall',
-	'Ahemmusa Camp',
-	'Erabenimsun Camp',
-	'Urshilaku Camp',
-	'Zainab Camp',
-	'Aharasaplit Camp',
-	'Sheogorad',
-	'Rotheran',
-	'Aidanat Camp',
-	'Ashamanu Camp',
-	'Bensiberib Camp',
-	'Ashlanders',
-	'Azura-s Coast',
-	'Tel Uvirith',
-	'Elanius Camp',
-	'Grazelands',
-	'Falensarano',
-	'Kaushtababi Camp',
-	'Mamshar-Disamus Camp',
-	'Kogoruhn',
-	'Massahanud Camp',
-	'Indoranyon',
-	'Mila-Nipal',
-	'Dushariran',
-	'Salothan Ancestral Tomb',
-	'Salit Camp',
-	'Shashmanu Camp',
-	'Shashurari Camp',
-	'Sobitbael Camp',
-	'Yakaridan Camp'
-}
+--Let's not give vived much higher chances of appearing
+vivecs= {'Vivec (Your pick at cantons)','Vivec Foreign','Vivec Hlaalu','Vivec Redoran','Vivec Telvanni','Vivec Arena','Vivec St. Olms','Vivec St. Delyn','Vivec Temple','Vivec Ministry','Palace of Vivec'}
+RANDOMVEHK = vivecs[math.random(1,11)]
 
-propercities = {
-	'Necrom',
-	'Ebon Tower',
-	'Port Telvannis',
-	'Firewatch',
-	'Gah Sadrith',
-	'Almas Thirr',
-	'Helnim',
-	'Aimrah',
-	'Bahrammu',
-	'Bal Oyra',
-	'Bosmora',
-	'Dondril',
-	'Enamor Dayn',
-	'Gorne',
-	'Ildrim',
-	'Indal-ruhn',
-	'Llothanis',
-	'Molagreahd',
-	'Meralag',
-	'Ranyon-ruhn',
-	'Sadas Plantation',
-	'Sailen',
-	'Tel Mothrivra',
-	'Mainland Tribunal Temple',
-	'Tower of Tel Mothrivra',
-	'Tel Muthada',
-	'Vhul',
-	'Ald-ruhn',
-	'Balmora',
-	'Sadrith Mora',
-	'Vivec ',
-	'Vvardenfell',
-	'Vivec Foreign',
-	'Vivec Hlaalu',
-	'House Hlaalu',
-	'Vivec Redoran',
-	'House Redoran',
-	'Vivec Telvanni',
-	'House Telvanni',
-	'Vivec Arena',
-	'Vivec St. Olms',
-	'Vivec St. Delyn',
-	'Tribunal Temple',
-	'Vivec Temple',
-	'Vivec Ministry',
-	'Ordinator',
-	'Palace of Vivec',
-	'Ald Velothi',
-	'Caldera',
-	'West Gash',
-	'Dagon Fel',
-	'Gnaar Mok',
-	'Gnisis',
-	'Hla Oad',
-	'Khuul',
-	'Maar Gan',
-	'Molag Mar',
-	'Pelagiad',
-	'Seyda Neen',
-	'Suran',
-	'Tel Aruhn',
-	'Tel Branora',
-	'Tel Fyr',
-	'Tel Mora',
-	'Tel Vos',
-	'Vos',
-	'Redoran',
-	'Bal Isra',
-	'Buckmoth Legion Fort',
-	'Ebonheart',
-	'Imperial',
-	'Moonmoth Legion Fort',
-	'Wolverine Hall',
-	'Ahemmusa Camp',
-	'Erabenimsun Camp',
-	'Urshilaku Camp',
-	'Zainab Camp'
-}
+print('What cities do you want to roll?')
+print()
+print('Vanilla and Tamriel Rebuilt:')
+print('(1) All settlements')
+print('(2) Only mayor settlements')
+print()
+print('Vanilla only:')
+print('(3) All settlements')
+print('(4) Only mayor settlements')
+print()
+print('Tamriel Rebuilt only:')
+print('(5) All settlements')
+print('(6) Only mayor settlements')
+print()
+local answer
+repeat
+   io.write("Pick an option: ")
+   io.flush()
+   answer=io.read()
+until answer=="1" or answer=="2" or answer=="3" or answer=="4" or answer=="5" or answer=="6"
+print()
+print()
+print()
+--every single city
+if( answer=="1") then
+cities = {'Necrom','Ebon Tower','Port Telvannis','Firewatch','Gah Sadrith','Almas Thirr','Helnim','Aimrah','Bahrammu','Bal Oyra','Bosmora','Dondril','Enamor Dayn','Gorne','Ildrim','Indal-ruhn','Llothanis','Molagreahd','Meralag','Ranyon-ruhn','Sadas Plantation','Sailen','Tel Mothrivra','Mainland Tribunal Temple','Tower of Tel Mothrivra','Tel Muthada','Vhul','Ammar','Cephorad Keep','Dreynim Spa','Erethan Plantation','Akamora','Fort Umbermoth','Nethril Plantation','Mephalain Mountains','Oran Plantation','Roa Dyr','Tel Aranyon','Master Mithras','Tel Narrusa','Narrusa Darythi','Vathras Plantation','Windmoth Legion Fort','Great House Telvanni','Acre of Saint Meris','Sacred Lands','Adurin-Ouaka','Akamora Mines','Alendu Plantation','Areno Varos House','Ashamul','Baldrahn','Boethian Mountains Lighthouse','Sarvanni','Delos Deralins House','Denaven Manor','Edhryn Farothrans Lighthouse','Sea of Ghosts','Evos','Felms Ithul','Galam and Mehra Telas House','Gol Mok','Halaran Camp','Harethim Manor','Bisandryon','Helerans Lodge','Hla Bulor','Hlersis','The Inn Between','Kellidan','Lan Orethan Outpost','Llaran Manor','Lumber Camp','Melethi Lighthouse','Lake Boethiah','Mundrethi Plantation','Slave Market','Orelu Plantation','Tel Ouada','Rethil Manor','Rilsoan','Romithren Monastery','Sacred Lands Sanctuary','Sadavel Plantation','Dreynim','Salvu Manor','Sarvanni Molecrab Farm','Molecrab','Boethiahs Spine','Sassanibats Yurt','Seitur','Nedothril','Sennahanib Camp','Serynthul','Old Ebonheart','Sinramen Monastery','Soramil and Rothera Galdavos House','Tahvel','Mephalan Vales','Tel Oren','Inner Sea','Mistress Faruna','Telvanni Library','Telvanni Waystation','Alt Bosara','Thalotheran Manor','Tharonen Manor','Lan Orethan','Tilmeth Manor','Alt Orethan','Tomaril Manor','Veralan Farm','Marog','Verulas Pass','Vlars Manor','Ald-ruhn','Balmora','Sadrith Mora','Vvardenfell','House Hlaalu','Tribunal Temple','Ald Velothi','Caldera','West Gash','Dagon Fel','Gnaar Mok','Gnisis','Hla Oad','Khuul','Maar Gan','Molag Mar','Pelagiad','Seyda Neen','Suran','Tel Aruhn','Tel Branora','Tel Fyr','Tel Mora','Tel Vos','Vos','Redoran','Bal Isra','Buckmoth Legion Fort','Ebonheart','Imperial','Moonmoth Legion Fort','Wolverine Hall','Ahemmusa Camp','Erabenimsun Camp','Urshilaku Camp','Zainab Camp','Aharasaplit Camp','Sheogorad','Rotheran','Aidanat Camp','Ashamanu Camp','Bensiberib Camp','Ashlanders','Azura-s Coast','Tel Uvirith','Elanius Camp','Grazelands','Falensarano','Kaushtababi Camp','Mamshar-Disamus Camp','Kogoruhn','Massahanud Camp','Indoranyon','Mila-Nipal','Dushariran','Salothan Ancestral Tomb','Salit Camp','Shashmanu Camp','Shashurari Camp','Sobitbael Camp','Yakaridan Camp',RANDOMVEHK}
+end
+
+--Only large cities
+if( answer=="2") then
+cities = {'Necrom','Ebon Tower','Port Telvannis','Firewatch','Gah Sadrith','Almas Thirr','Helnim','Aimrah','Bahrammu','Bal Oyra','Bosmora','Dondril','Enamor Dayn','Gorne','Ildrim','Indal-ruhn','Llothanis','Molagreahd','Meralag','Ranyon-ruhn','Sadas Plantation','Sailen','Tel Mothrivra','Mainland Tribunal Temple','Tower of Tel Mothrivra','Tel Muthada','Vhul','Ald-ruhn','Balmora','Sadrith Mora','House Hlaalu','Tribunal Temple','Ald Velothi','Caldera','West Gash','Dagon Fel','Gnaar Mok','Gnisis','Hla Oad','Khuul','Maar Gan','Molag Mar','Pelagiad','Seyda Neen','Suran','Tel Aruhn','Tel Branora','Tel Fyr','Tel Mora','Tel Vos','Vos','Redoran','Bal Isra','Buckmoth Legion Fort','Ebonheart','Imperial','Moonmoth Legion Fort','Wolverine Hall','Ahemmusa Camp','Erabenimsun Camp','Urshilaku Camp','Zainab Camp',RANDOMVEHK}
+end
+--every single city in vanilla
+if( answer=="3") then
+cities = {'Ald-ruhn','Balmora','Sadrith Mora','Vvardenfell','House Hlaalu','Tribunal Temple','Ald Velothi','Caldera','West Gash','Dagon Fel','Gnaar Mok','Gnisis','Hla Oad','Khuul','Maar Gan','Molag Mar','Pelagiad','Seyda Neen','Suran','Tel Aruhn','Tel Branora','Tel Fyr','Tel Mora','Tel Vos','Vos','Redoran','Bal Isra','Buckmoth Legion Fort','Ebonheart','Imperial','Moonmoth Legion Fort','Wolverine Hall','Ahemmusa Camp','Erabenimsun Camp','Urshilaku Camp','Zainab Camp','Aharasaplit Camp','Sheogorad','Rotheran','Aidanat Camp','Ashamanu Camp','Bensiberib Camp','Ashlanders','Azura-s Coast','Tel Uvirith','Elanius Camp','Grazelands','Falensarano','Kaushtababi Camp','Mamshar-Disamus Camp','Kogoruhn','Massahanud Camp','Indoranyon','Mila-Nipal','Dushariran','Salothan Ancestral Tomb','Salit Camp','Shashmanu Camp','Shashurari Camp','Sobitbael Camp','Yakaridan Camp',RANDOMVEHK}
+end
+
+--Only large cities from vanilla
+if( answer=="4") then
+cities = {'Ald-ruhn','Balmora','Sadrith Mora','Vvardenfell','House Hlaalu','Tribunal Temple','Ald Velothi','Caldera','West Gash','Dagon Fel','Gnaar Mok','Gnisis','Hla Oad','Khuul','Maar Gan','Molag Mar','Pelagiad','Seyda Neen','Suran','Tel Aruhn','Tel Branora','Tel Fyr','Tel Mora','Tel Vos','Vos','Redoran','Bal Isra','Buckmoth Legion Fort','Ebonheart','Imperial','Moonmoth Legion Fort','Wolverine Hall','Ahemmusa Camp','Erabenimsun Camp','Urshilaku Camp','Zainab Camp',RANDOMVEHK}
+end
+--every single city from TR
+if( answer=="5") then
+cities = {'Necrom','Ebon Tower','Port Telvannis','Firewatch','Gah Sadrith','Almas Thirr','Helnim','Aimrah','Bahrammu','Bal Oyra','Bosmora','Dondril','Enamor Dayn','Gorne','Ildrim','Indal-ruhn','Llothanis','Molagreahd','Meralag','Ranyon-ruhn','Sadas Plantation','Sailen','Tel Mothrivra','Mainland Tribunal Temple','Tower of Tel Mothrivra','Tel Muthada','Vhul','Ammar','Cephorad Keep','Dreynim Spa','Erethan Plantation','Akamora','Fort Umbermoth','Nethril Plantation','Mephalain Mountains','Oran Plantation','Roa Dyr','Tel Aranyon','Master Mithras','Tel Narrusa','Narrusa Darythi','Vathras Plantation','Windmoth Legion Fort','Great House Telvanni','Acre of Saint Meris','Sacred Lands','Adurin-Ouaka','Akamora Mines','Alendu Plantation','Areno Varos House','Ashamul','Baldrahn','Boethian Mountains Lighthouse','Sarvanni','Delos Deralins House','Denaven Manor','Edhryn Farothrans Lighthouse','Sea of Ghosts','Evos','Felms Ithul','Galam and Mehra Telas House','Gol Mok','Halaran Camp','Harethim Manor','Bisandryon','Helerans Lodge','Hla Bulor','Hlersis','The Inn Between','Kellidan','Lan Orethan Outpost','Llaran Manor','Lumber Camp','Melethi Lighthouse','Lake Boethiah','Mundrethi Plantation','Slave Market','Orelu Plantation','Tel Ouada','Rethil Manor','Rilsoan','Romithren Monastery','Sacred Lands Sanctuary','Sadavel Plantation','Dreynim','Salvu Manor','Sarvanni Molecrab Farm','Molecrab','Boethiahs Spine','Sassanibats Yurt','Seitur','Nedothril','Sennahanib Camp','Serynthul','Old Ebonheart','Sinramen Monastery','Soramil and Rothera Galdavos House','Tahvel','Mephalan Vales','Tel Oren','Inner Sea','Mistress Faruna','Telvanni Library','Telvanni Waystation','Alt Bosara','Thalotheran Manor','Tharonen Manor','Lan Orethan','Tilmeth Manor','Alt Orethan','Tomaril Manor','Veralan Farm','Marog','Verulas Pass','Vlars Manor'}
+end
+
+--Only large cities from TR
+if( answer=="6") then
+cities = {'Necrom','Ebon Tower','Port Telvannis','Firewatch','Gah Sadrith','Almas Thirr','Helnim','Aimrah','Bahrammu','Bal Oyra','Bosmora','Dondril','Enamor Dayn','Gorne','Ildrim','Indal-ruhn','Llothanis','Molagreahd','Meralag','Ranyon-ruhn','Sadas Plantation','Sailen','Tel Mothrivra','Mainland Tribunal Temple','Tower of Tel Mothrivra','Tel Muthada','Vhul'}
+end
 
 stars = {
 	'Warrior',
@@ -457,4 +239,5 @@ shuffle(factions)
 print('',shuffled[1])
 print('',shuffled[2])
 --cityrand= math.random(119~190) --No tamriel rebuilt
-print('City:',cities[math.random(1,190)])
+shuffle(cities)
+print('City:',shuffled[1])
